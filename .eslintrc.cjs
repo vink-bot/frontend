b4221f62529034bb0e8912b35840308cbdac9744
Дмитрий Типсин <tipsin.dm@yandex.ru>
2024-03-31 21:44:24 +0300

    chore: Настройки форматирования кода

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,23 +2,25 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "prettier"
   ],
 
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    quotes: ['error', 'single'],
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "max-len": ["warn", { code: 180, ignoreComments: true }],
+    quotes: ["error", "single"],
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true }
     ],
-    'no-unused-vars': ['off'],
-    '@typescript-eslint/no-unused-vars': ['off'],
-  },
+    "no-unused-vars": ["off"],
+    "@typescript-eslint/no-unused-vars": ["off"]
+  }
 };
