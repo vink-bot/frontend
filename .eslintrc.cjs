97e7f0aabbe37b527366b66df564c31cf32e4320
Дмитрий Типсин <tipsin.dm@yandex.ru>
2024-04-06 12:49:24 +0300

    fix: Line separator

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,28 +2,28 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
-    "prettier"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
-  ignorePatterns: ["dist"],
-  parser: "@typescript-eslint/parser",
+  ignorePatterns: ['dist'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: { jsx: true },
     ecmaVersion: 12,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "max-len": "off",
-    quotes: ["error", "single"],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "prettier/prettier": "error",
-    "@typescript-eslint/no-unused-vars": "off" // Отключение правила
-  }
+    'react/react-in-jsx-scope': 'off',
+    'max-len': 'off',
+    quotes: ['error', 'single'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'off', // Отключение правила
+  },
 };
